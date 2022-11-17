203cfc88c0fd493b0631580b3374f4b706e5080c
liying <liying@lying>
2022-11-17 17:27:44 +0800

    add log

---
+++ src/main/java/tool/util/ExceptionUtil.java
@@ -0,0 +1,15 @@
+package tool.util;
+
+/**
+ * @className: ExceptionUtil
+ * @author: Lying
+ * @description: TODO
+ * @date: 2022/11/17 下午5:03
+ */
+public class ExceptionUtil extends Exception{
+    public ExceptionUtil(String s) {
+        super(s);
+
+
+    }
+}
---
+++ src/main/java/tool/util/LogUtil.java
@@ -0,0 +1,19 @@
+package tool.util;
+
+import com.sun.source.doctree.SerialDataTree;
+
+import java.io.Serializable;
+
+/**
+ * @className: LogUtil
+ * @author: Lying
+ * @description: TODO
+ * @date: 2022/11/17 下午5:10
+ */
+@SuppressWarnings("unused")
+public class LogUtil implements Serializable {
+
+    private static final long serialVersionUID = 3497418501502482083L;
+
+
+}
